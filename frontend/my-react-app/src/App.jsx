9477077b6058653b89bd597f79e4c9d810bcb8dc
dkiltinas <kiltinasdeividas@gmail.com>
2024-05-24 20:59:41 +0300

    Routing setup for pages navigation completed

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import NavBar from "../components/NavBar";
+import HomePage from "../pages/HomePage";
+import AddMoviePage from "../pages/AddMoviePage";
+import EditMoviePage from "../pages/EditMoviePage";
+import SearchMoviesPage from "../pages/SearchMoviesPage";
+// Setup routes
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/add-movie" component={AddMoviePage} />
+          <Route path="/edit-movie/:id" component={EditMoviePage} />
+          <Route path="/search-movies" component={SearchMoviesPage} />
+        </Switch>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
